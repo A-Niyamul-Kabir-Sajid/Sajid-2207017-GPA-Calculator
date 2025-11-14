f605ea29f5875fb9282d17ac28db8113f2f2294c
A-Niyamul-Kabir-Sajid <sajidbangla893@gmail.com>
2025-11-14 15:31:15 +0600

    first comment

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.sajid.sajid_2207017_gpa_calculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.sajid.sajid_2207017_gpa_calculator to javafx.fxml;
+    exports com.sajid.sajid_2207017_gpa_calculator;
+}
